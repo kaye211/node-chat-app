8f0a37ed0b2b546d0dc4e087f9217d55b1638b44
Kaye <kay22ng@yahoo.com>
2018-09-01 15:36:16 -0500

    Add connection event handlers

--- server/server.js
+++ server/server.js
@@ -1,13 +1,26 @@
 const path = require('path');
+
+const http = require('http');
 const publicPath = path.join(__dirname, '../public');
 const express = require('express');
-
+const socketIO = require('socket.io');
 const port = process.env.PORT || 3000;
 const app = express();
 
+var server = http.createServer(app);
+//configure for socketio
+var io = socketIO(server);
 //configure express static middleware
 app.use(express.static(publicPath));
 //start up server
-app.listen(port, () => {
+
+io.on('connection', socket => {
+	console.log('New connection');
+
+	socket.on('disconnect', () => {
+		console.log('disconnected from client');
+	});
+});
+server.listen(port, () => {
 	console.log(`Server started at ${port} `);
 });
