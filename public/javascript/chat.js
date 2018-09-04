f19092b784b6236c245932733c94b1341a97d51d
Kaye <kay22ng@yahoo.com>
2018-09-04 06:39:27 -0500

    renamed index.html and index.js and added index.html page

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -1,19 +1,28 @@
 var socket = io();
 
+function scrollToBottom() {
+	// Selectors
+	var messages = jQuery('#messages');
+	var newMessage = messages.children('li:last-child');
+	// Heights
+	var clientHeight = messages.prop('clientHeight');
+	var scrollTop = messages.prop('scrollTop');
+	var scrollHeight = messages.prop('scrollHeight');
+	var newMessageHeight = newMessage.innerHeight();
+	var lastMessageHeight = newMessage.prev().innerHeight();
+
+	if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+		messages.scrollTop(scrollHeight);
+	}
+}
+
 socket.on('connect', function() {
 	console.log('Connected to server ');
-	// socket.emit('createEmail', {
-	// 	to: 'Paul',
-	// 	text: 'Hey',
-	// });
 });
 socket.on('disconnect', function() {
 	console.log('Disconnected from server');
 });
 
-// socket.on('newEmail', function(email) {
-// 	console.log('New Email', email);
-// });s
 socket.on('newMessage', function(message) {
 	var formattedTime = moment(message.createdAt).format('h:mm a');
 	var template = jQuery('#message-template').html();
@@ -24,6 +33,7 @@ socket.on('newMessage', function(message) {
 	});
 
 	jQuery('#messages').append(html);
+	scrollToBottom();
 
 	// var li = jQuery('<li></li>');
 	// li.text(`${message.from}:${formattedTime} ${message.text}`);
@@ -40,13 +50,7 @@ socket.on('newLocationMessage', function(message) {
 	});
 
 	jQuery('#messages').append(html);
-	// var li = jQuery('<li></li>');
-	// var a = jQuery('<a target="_blank">My current location</a>');
-
-	// li.text(`${message.from}:${formattedTime} `);
-	// a.attr('href', message.url);
-	// li.append(a);
-	// jQuery('#messages').append(li);
+	scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function(e) {
